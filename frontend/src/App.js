fc5ade3f6904e288193e7533135dccf772bf7c9e
Yevgeniy Ulchenkov <ulcheyev@fel.cvut.cz>
2023-06-21 21:20:51 +0200

    add compose file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project Portfolio Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    clients: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8080/api/v1/system/users');
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    const {clients} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Clients</h2>
+              {clients.map(client =>
+                  <div key={client.id}>
+                    {client.username}
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
