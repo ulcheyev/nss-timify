376923a19c07049bc0a568eac4268e6bc213d9a6
Yevgeniy Ulchenkov <ulcheyev@fel.cvut.cz>
2023-06-24 20:06:07 +0200

    cors solve

--- backend/api-gateway/src/main/java/com/kyki/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/kyki/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.kyki.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,4 +12,19 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+
 }
+
