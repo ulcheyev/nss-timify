36d629f6212987e5c1cee05d552852e62827722f
Yevgeniy Ulchenkov <ulcheyev@fel.cvut.cz>
2023-06-25 17:36:48 +0200

    add legacy support

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://34.125.160.101:8080/api/v1/system/users';
+const API_URL = 'http://34.125.160.101:8080/api/v1/system/';
 
 export class UserService {
     login(user){
