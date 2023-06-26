316b3d6521d73c388073df51649cbc95e915cc04
Yevgeniy Ulchenkov <ulcheyev@fel.cvut.cz>
2023-06-26 16:38:42 +0200

    fix origins v2

--- backend/user-microservice/src/main/java/com/kyki/usermicroservice/controller/AuthController.java
+++ backend/user-microservice/src/main/java/com/kyki/usermicroservice/controller/AuthController.java
@@ -3,10 +3,9 @@ package com.kyki.usermicroservice.controller;
 import com.kyki.usermicroservice.dto.AuthRequest;
 import com.kyki.usermicroservice.dto.AuthResponse;
 import com.kyki.usermicroservice.service.AuthService;
-import com.kyki.usermicroservice.utils.TimifyUtils;
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
@@ -15,15 +14,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping(path = "api/v1/system/login")
-@RequiredArgsConstructor
 @CrossOrigin
 public class AuthController {
-
+    private HttpServletRequest request2;
     private final AuthService authenticationService;
+
+    @Autowired
+    public AuthController(HttpServletRequest request, AuthService authenticationService) {
+        this.request2 = request;
+        this.authenticationService = authenticationService;
+    }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthResponse> authenticate(@RequestBody @NonNull AuthRequest request) {
+        String remoteAddr = request2.getRemoteAddr();
         log.info("AuthController-authenticate: " + request);
+        log.info("Ip: " + remoteAddr);
         return ResponseEntity.ok().body(authenticationService.authenticate(request));
     }
 }
